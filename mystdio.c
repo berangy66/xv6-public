48ee60f7270ad13cf1eebdf21c9a2c42610e8472
Behrang Moradi <moradib@uwindsor.ca>
2023-02-20 11:33:16 -0500

    a2-needs modifing

--- mystdio.c
+++ mystdio.c
@@ -4,34 +4,38 @@
 #include "fcntl.h"
 #include "mystdio.h"
 
-
-FILE stdin0 = {.fd = 0, .mode = 0, .buffer_current_positon = 0};
+/****************************************************************************************************************************************************************/
+/**** FILE (*mystdin, *mystdout, & *mystderro), are pre-defined and implicitly opened upon the execution of any of user program ************/
+FILE stdin0 = {.fd = 0, .mode = 0, .buffer_current_positon = 0 };
 FILE *mystdin = &stdin0;
-FILE stdout1 = {.fd = 1, .mode = 0, .buffer_current_positon = 0};
+FILE stdout1 = {.fd = 1, .mode = 0, .buffer_current_positon = 0 };
 FILE *mystdout = &stdout1;
 FILE stderr2 = {.fd = 2, .mode = 0, .buffer_current_positon = 0};
 FILE *mystderr = &stderr2;
+/****************************************************************************************************************************************************************/
 
-// mode 'r' to read & mode 'w' to write
+
+/****************************************************************************************************************************************************************/
+// ***** mode 'r' to read & mode 'w' to write ******//
 FILE *myfopen(const char *path, char mode)
 {
   int fd1;
   FILE *fp = (FILE *)malloc(sizeof(FILE));
-  int m;
+  int mode_value;
   if (mode == 'r')
   {
-    m = O_RDONLY;
+    mode_value = O_RDONLY;
   }
   else if (mode == 'w')
   {
-    m = O_WRONLY | O_CREATE;
+    mode_value = O_WRONLY | O_CREATE;
   }
   else
   {
     printf(2, "You have entered invalid mode\n");
     exit();
   }
-  fd1 = open(path, m);
+  fd1 = open(path, mode_value);
   if (fd1 < 0)
   {
     printf(2, "could not open a file to Read or Write!\n");
@@ -39,58 +43,71 @@ FILE *myfopen(const char *path, char mode)
   }
 
   fp->fd = fd1;
-  fp->mode = m;
+  fp->mode = mode_value;
   fp->buffer_current_positon = 0;
   //memset(fp->mybuffer, 0, BUFF_SIZE); // set buffer to 0
-
   return fp;
-
 } // END FUNCTION *myfopen
+/****************************************************************************************************************************************************************/
 
-/******** fluhes buffer  **********/
+
+/****************************************************************************************************************************************************************/
+/******** myclose funcitons calls myffluhes to flush buffer , close & free memory **********/
 void myclose(FILE *fp)
 {
-  close(fp->fd);
-  exit();
+  myfflush(fp); 
+    close(fp->fd);
+    free(fp);
+
+ 
 
 } // END FUNCTION myclose
+/****************************************************************************************************************************************************************/
 
-void myfflush(FILE* fp)
+
+/****************************************************************************************************************************************************************/
+/******** fluhes buffer will write out content of buffer, sets current_buffers_position back to zero **********/
+void myfflush(FILE *fp)
 {
-  // flush the buffer
-}
+  if(fp->buffer_current_positon > 0)
+  {
+    write(fp->fd, fp->mybuffer, fp->buffer_current_positon); 
+    fp->buffer_current_positon = 0; 
+  }
+   write(fp->fd, fp->mybuffer, fp->buffer_current_positon); 
+   close(fp->fd);
+}//END FUNCTION myfflush 
+/****************************************************************************************************************************************************************/
 
+
+/****************************************************************************************************************************************************************/
 /****** it's fully buffered and handles %d, %x, %p and %s **********/
 /****** it works with file pointers instead of file descriptor *********/
 /****** uses constant BUFF_SIZE *************/
-void myfprintf1(FILE *fp, const char *s, ...)
-{
-
-  int nbytesRD = 0;
-  int nbytesWR = 0;
-  int fd = fp->fd;
-
-  printf(1, "i am in myfprintf block %d\n", fd);
-  strcpy(fp->mybuffer, s);
-  while ((nbytesRD = read(fd, fp->mybuffer, sizeof(BUFF_SIZE))) > 0)
-  {
-
-    nbytesWR = write(fd, fp->mybuffer, BUFF_SIZE);
-  }
-
-  fp->buffer_current_positon = nbytesWR;
-  // fp->mybuffer[nbytesWR];
-  // close(fp->fd);
-  exit();
-
-} // END FUNCTION myfprintf
-
 static void
 myputc(FILE *fp, char c)
 {
-  write(fp->fd, &c, 1);
-}
+  // TA's suggestion to make sure its buffere
+  //  write(fp->fd, strcpy(fp->mybuffer ,&c), 1);
+  //     if(strlen(fp->mybuffer) == BUFF_SIZE)
+  //   {
+  //     free(fp);
+  //     close(fp->fd); 
+  //   }  
 
+  if(fp->buffer_current_positon == BUFF_SIZE)
+  {
+    write(fp->fd, fp->mybuffer, BUFF_SIZE);
+    fp->buffer_current_positon = 0;
+    //myfflush(fp); 
+  } 
+  fp->mybuffer[fp->buffer_current_positon] = c; 
+  fp->buffer_current_positon++; 
+}//END FUNCTION myputc
+/****************************************************************************************************************************************************************/
+
+
+/****************************************************************************************************************************************************************/
 static void
 myfprintint(FILE *fp, int xx, int base, int sgn)
 {
@@ -120,8 +137,11 @@ myfprintint(FILE *fp, int xx, int base, int sgn)
 
   while (--i >= 0)
     myputc(fp, buf[i]);
-}
+}//END FUNCTION myfprintint()
+/****************************************************************************************************************************************************************/
 
+
+/****************************************************************************************************************************************************************/
 // Print to the given fd. Only understands %d, %x, %p, %s.
 void myfprintf(FILE *fp, const char *fmt, ...)
 {
@@ -187,4 +207,5 @@ void myfprintf(FILE *fp, const char *fmt, ...)
       state = 0;
     }
   }
-}
+}//END FUNCTION myfprintf()
+/****************************************************************************************************************************************************************/
--- mystdiotest.c
+++ mystdiotest.c
@@ -7,12 +7,24 @@
 /* FUNCTION MAIN STARTS */
 int main(int argc, char *argv[])
 {
-  // printf(1,"jamie said hello\n");
+
   FILE *fp;
   fp = myfopen("test.txt", 'w');
-  myfprintf(mystdout, "my fp value is %d\n", fp);
 
-  myfprintf(fp, "%s", "hello Behrang!");
+  myfprintf(mystdout, "my fp value is %d\n", fp);
+  int starttime = 0;
+  int endtime = 0;
+
+
+  for (int i = 0; i < sizeof(fp->mybuffer); i++)
+  {
+   
+   
+      myfprintf(fp, "\n[%d] starttime: [%d] %s endtime[%d]", i, starttime = uptime(), "hello Xv6!..  ", endtime = uptime());
+      myfprintf(mystdout,"%p Xv6\t",   fp->buffer_current_positon);
+      myfflush(fp); // flush the buffer to write data to the file
+  }
+
   close(fp->fd);
   exit();
 } // END MAIN FUNCTION
